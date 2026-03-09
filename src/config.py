b516bc575ae328dcd35587f247b85df561119594
coza_nostra7 <begzodmuminovb@gmail.com>
2026-03-09 22:15:58 +0100

    created configurations for database , and JWT token configurations

---
+++ src/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings,SettingsConfigDict
+
+class Settings(BaseSettings):
+    DB_NAME:str
+    DB_HOST: str
+    DB_PORT: int
+    DB_PASS: str
+    DB_USER:str
+
+
+    @property
+    def DB_URL(self):
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES:int
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+settings = Settings()
