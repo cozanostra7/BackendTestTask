b0fc24957a7881bacbc8a04c11c956d7edad2f4e
coza_nostra7 <begzodmuminovb@gmail.com>
2026-03-10 21:25:32 +0100

    Created dependencies for users

---
+++ src/api/dependencies.py
@@ -0,0 +1,15 @@
+from fastapi import Request, Depends, HTTPException
+from src.services.auth import AuthService
+
+
+
+def get_token(request: Request) -> str:
+    token = request.cookies.get("access_token")
+    if not token:
+        raise HTTPException(status_code=401, detail="You are not authorized")
+    return token
+
+def get_current_user(token: str = Depends(get_token)):
+    auth_service = AuthService()
+    payload = auth_service.decode_token(token)
+    return {"username": payload["sub"], "role": payload["role"]}
