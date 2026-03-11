b891a2c4bed26c8da825998fcefd4054a86a3dac
coza_nostra7 <begzodmuminovb@gmail.com>
2026-03-11 16:14:14 +0100

    updated dependencies so that authorization is not needed

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -3,13 +3,21 @@ from src.services.auth import AuthService
 
 
 
-def get_token(request: Request) -> str:
-    token = request.cookies.get("access_token")
-    if not token:
-        raise HTTPException(status_code=401, detail="You are not authorized")
-    return token
+# def get_token(request: Request) -> str:
+#     token = request.cookies.get("access_token")
+#     if not token:
+#         raise HTTPException(status_code=401, detail="You are not authorized")
+#     return token
+#
+# def get_current_user(token: str = Depends(get_token)):
+#     auth_service = AuthService()
+#     payload = auth_service.decode_token(token)
+#     return {"username": payload["sub"], "role": payload["role"]}
 
-def get_current_user(token: str = Depends(get_token)):
-    auth_service = AuthService()
-    payload = auth_service.decode_token(token)
-    return {"username": payload["sub"], "role": payload["role"]}
+
+
+def getCurrentUser():
+
+    return {"username": "admin", "role": "admin"}
+
+
